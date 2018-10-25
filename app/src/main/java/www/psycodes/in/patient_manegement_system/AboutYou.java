f0e9e33171cef15a2ce4cc1c9715fd999f84cf56
Clitan <42905022+Clitan@users.noreply.github.com>
2018-10-25 23:14:28 +0530

    commit AboutYou and Addpatient

--- app/src/main/java/www/psycodes/in/patient_manegement_system/AboutYou.java
+++ app/src/main/java/www/psycodes/in/patient_manegement_system/AboutYou.java
@@ -1,26 +1,72 @@
 package www.psycodes.in.patient_manegement_system;
 
 import android.content.Intent;
-import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.support.v7.app.AppCompatActivity;
 import android.view.View;
 import android.widget.Button;
+import android.widget.EditText;
 
 public class  AboutYou extends AppCompatActivity {
     Button aboutYouNext;
+    EditText First_Name;
+    EditText Last_Name;
+    EditText Dob;
+    EditText Speciallization;
+    EditText Hospital;
+    EditText Landmark;
+    EditText City;
+    EditText Pincode;
+    String Fname;
+    String Lname;
+    String dob;
+    String Spec;
+    String hospital;
+    String landmark;
+    String city;
+    String pincode;
+
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_about_you);
-        aboutYouNext=findViewById(R.id.about_you_next_button);
-    }
 
-    public void homePage(View view) {
+        aboutYouNext = findViewById(R.id.about_you_next_button);
+        First_Name = findViewById(R.id.about_you_first_name);
+        Last_Name = findViewById(R.id.about_you_last_name);
+        Dob  = findViewById(R.id.about_you_dob);
+        Speciallization =findViewById(R.id.about_you_specialization);
+        Hospital = findViewById(R.id.about_you_hospital_name);
+        Landmark = findViewById(R.id.about_you_land_mark);
+        City = findViewById(R.id.about_you_city);
+        Pincode = findViewById(R.id.about_you_pincode);
+
+
         aboutYouNext.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View v) {
+                data();
+                if(Fname.length() != 0 && hospital.length() != 0 && city.length() != 0 && pincode.length() != 0 ){
                 startActivity(new Intent(AboutYou.this,HomePage.class));
             }
+        }
+
         });
+
+
     }
+
+    private void data() {
+        Fname = First_Name.getText().toString().trim();
+        Lname = Last_Name.getText().toString().trim();
+        dob = Dob.getText().toString().trim();
+        Spec = Speciallization.getText().toString().trim();
+        hospital = Hospital.getText().toString().trim();
+        landmark = Landmark.getText().toString().trim();
+        city = City.getText().toString().trim();
+        pincode = Pincode.getText().toString().trim();
+
+    }
+
+
 }
--- app/src/main/java/www/psycodes/in/patient_manegement_system/AddPatient.java
+++ app/src/main/java/www/psycodes/in/patient_manegement_system/AddPatient.java
@@ -1,28 +1,72 @@
 package www.psycodes.in.patient_manegement_system;
 
 import android.content.Intent;
-import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.support.v7.app.AppCompatActivity;
 import android.view.View;
 import android.widget.Button;
+import android.widget.EditText;
 import android.widget.Toast;
 
 public class AddPatient extends AppCompatActivity {
     Button addPatientDoneButton;
+
+    EditText First_Name;
+    EditText Last_Name;
+    EditText Dob;
+    EditText Landmark;
+    EditText City;
+    EditText Pincode;
+    EditText Disease;
+    EditText Symptom;
+
+    String Fname;
+    String Lname;
+    String dob;
+    String symptom;
+    String disease;
+    String landmark;
+    String city;
+    String pincode;
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_add_patient);
         addPatientDoneButton=findViewById(R.id.add_patient_add_button);
-    }
 
-    public void successfulAddPatient(View view) {
+        First_Name = findViewById(R.id.add_patient_first_name);
+        Last_Name = findViewById(R.id.add_patient_last_name);
+        Dob  = findViewById(R.id.add_patient_dob);
+        Landmark = findViewById(R.id.add_patient_landmark);
+        City = findViewById(R.id.add_patient_city);
+        Pincode = findViewById(R.id.add_patient_pincode);
+        Disease = findViewById(R.id.add_patient_disease);
+        Symptom = findViewById(R.id.add_patient_symptom);
+
+
+
         addPatientDoneButton.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View v) {
-                Toast.makeText(AddPatient.this, "Patient Added Successfully", Toast.LENGTH_SHORT).show();
-                startActivity(new Intent(AddPatient.this,PatientDetails.class));
+                data();
+                if(Fname.length() != 0 && Disease.length()!= 0 ){
+                    Toast.makeText(AddPatient.this, "Patient Added Successfully", Toast.LENGTH_SHORT).show();
+                    startActivity(new Intent(AddPatient.this,PatientDetails.class));
+                }
             }
+
         });
     }
+
+    private void data() {
+        Fname = First_Name.getText().toString().trim();
+        Lname = Last_Name.getText().toString().trim();
+        dob = Dob.getText().toString().trim();
+        symptom = Symptom.getText().toString().trim();
+        disease = Disease.getText().toString().trim();
+        landmark = Landmark.getText().toString().trim();
+        city = City.getText().toString().trim();
+        pincode = Pincode.getText().toString().trim();
+    }
+
 }
