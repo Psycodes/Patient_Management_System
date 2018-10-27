967d4c86b11c936a62caa1d4afe5876ad9172c17
Clitan <42905022+Clitan@users.noreply.github.com>
2018-10-27 14:20:02 +0530

    commit SplashScreen

--- app/src/main/java/www/psycodes/in/patient_manegement_system/AboutYou.java
+++ app/src/main/java/www/psycodes/in/patient_manegement_system/AboutYou.java
@@ -8,16 +8,22 @@ import android.widget.Button;
 import android.widget.EditText;
 import android.widget.Toast;
 
+import java.io.FileOutputStream;
+import java.io.IOException;
+import java.util.Arrays;
+
 public class  AboutYou extends AppCompatActivity {
     Button aboutYouNext;
+
     EditText First_Name;
     EditText Last_Name;
     EditText Dob;
-    EditText Speciallization;
+    EditText Specialization;
     EditText Hospital;
     EditText Landmark;
     EditText City;
     EditText Pincode;
+
     String Fname;
     String Lname;
     String dob;
@@ -26,7 +32,11 @@ public class  AboutYou extends AppCompatActivity {
     String landmark;
     String city;
     String pincode;
+    String text;
+
     int flag=0;
+
+    private static final String FILE_NAME = "DOCTOR_INFO.txt";
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -37,12 +47,11 @@ public class  AboutYou extends AppCompatActivity {
         First_Name = findViewById(R.id.about_you_first_name);
         Last_Name = findViewById(R.id.about_you_last_name);
         Dob  = findViewById(R.id.about_you_dob);
-        Speciallization =findViewById(R.id.about_you_specialization);
+        Specialization =findViewById(R.id.about_you_specialization);
         Hospital = findViewById(R.id.about_you_hospital_name);
         Landmark = findViewById(R.id.about_you_land_mark);
         City = findViewById(R.id.about_you_city);
         Pincode = findViewById(R.id.about_you_pincode);
-
 
         aboutYouNext.setOnClickListener(new View.OnClickListener() {
             @Override
@@ -50,24 +59,33 @@ public class  AboutYou extends AppCompatActivity {
                 data();
                 if(Fname.length() != 0 && hospital.length() != 0 && city.length() != 0 && pincode.length() != 0 ){
                 startActivity(new Intent(AboutYou.this,HomePage.class));
+                }
             }
-        }
-
         });
-
-
     }
 
     private void data() {
-        Fname = First_Name.getText().toString().trim();
-        Lname = Last_Name.getText().toString().trim();
-        dob = Dob.getText().toString().trim();
-        Spec = Speciallization.getText().toString().trim();
-        hospital = Hospital.getText().toString().trim();
-        landmark = Landmark.getText().toString().trim();
-        city = City.getText().toString().trim();
-        pincode = Pincode.getText().toString().trim();
+        Fname = First_Name.getText().toString();
+        Lname = Last_Name.getText().toString();
+        dob = Dob.getText().toString();
+        Spec = Specialization.getText().toString();
+        hospital = Hospital.getText().toString();
+        landmark = Landmark.getText().toString();
+        city = City.getText().toString();
+        pincode = Pincode.getText().toString();
 
+        text = "       ABOUT_YOU          \n---------------------------------\nFIRST NAME:"+ Arrays.toString(Fname.toUpperCase().getBytes()) +"\nLAST NAME:"+ Arrays.toString(Lname.toUpperCase().getBytes()) + "\nDATE-OF-BIRTH:"+ Arrays.toString(dob.toUpperCase().getBytes()) +"\n"+
+               "SPECIALIZATION:"+ Arrays.toString(Spec.toUpperCase().getBytes()) +"\nHOSPITAL:"+ Arrays.toString(hospital.toUpperCase().getBytes()) + "\nLANDMARK:"+ Arrays.toString(landmark.toUpperCase().getBytes()) +"\n"+
+               "CITY:"+ Arrays.toString(city.toUpperCase().getBytes()) +"\nPINCODE:"+ Arrays.toString(pincode.toUpperCase().getBytes()) +"\n";
+
+        try {
+            FileOutputStream fileOutputStream;
+            fileOutputStream = openFileOutput(FILE_NAME,MODE_PRIVATE);
+            fileOutputStream.write(text.getBytes());
+            Toast.makeText(this, "saved", Toast.LENGTH_SHORT).show();
+        } catch (IOException e) {
+            e.printStackTrace();
+        }
     }
 
 
@@ -76,9 +94,8 @@ public class  AboutYou extends AppCompatActivity {
             Toast.makeText(AboutYou.this, "Press again to exit", Toast.LENGTH_SHORT).show();
             flag =1;
         }
-//        if(flag == 1){
-//            //here code to exit the app
-//        }
-
+        if(flag == 1){
+            finish();
+        }
     }
 }
